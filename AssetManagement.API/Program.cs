203a38797b82129390459853bd87652b28a0ff0c
Jordan Lima <jordan.lima@grendene.com.br>
2025-09-16 08:10:47 -0300

    feat: Integrar autenticação JWT à API.

--- AssetManagement.API/Program.cs
+++ AssetManagement.API/Program.cs
@@ -1,12 +1,32 @@
+using System.Text;
 using AssetManagement.Application.Interfaces;
 using AssetManagement.Application.Services;
 using AssetManagement.Domain.Repositories;
 using AssetManagement.Domain.Services;
 using AssetManagement.Infrastructure.Persistence;
 using AssetManagement.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("minha_chave_secreta_super_segura_1234567890"))
+    };
+});
 
 builder.Services.AddDbContext<AssetManagementDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -51,6 +71,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
